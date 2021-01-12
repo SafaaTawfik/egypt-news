9e0b729a0060eddeb15585c26e08a2792a9e4357
Safaa Mohamed <safaatwfik@yahoo.com>
2021-01-12 22:28:29 +0200

    add header and footer and modify the interface

--- src/app.js
+++ src/app.js
@@ -5,31 +5,41 @@ const app = express();
 const newsapi = require("./tools/newsapi");
 const { response } = require("express");
 const port = 3000;
-
-//serving static files
-
-const publicDir = path.join(__dirname, "../public");
-app.use(express.static(publicDir));
-
-// setup hbs as view engine
+// set  hbs as view engine
 app.set("view engine", "hbs");
 
-//
-
+// Set up view path
 const viewsPath = path.join(__dirname, "../src/views");
-
 app.set("views", viewsPath);
 
 //Register Partial files
-const partialsPath = path.join(__dirname, "../partials");
+const partialsPath = path.join(__dirname, "../src/partials");
 hbs.registerPartials(partialsPath);
+
+//serving static files
+const publicDir = path.join(__dirname, "../public");
+app.use(express.static(publicDir));
 
 app.get("", (req, res) => {
   newsapi((error, data) => {
     if (error) {
-      return res.send({ error });
+      res.render("404page", {
+        title: "404 Error",
+        name: "error page",
+      });
     }
-    res.render("index", { news: data });
+    res.render("index", {
+      news: data,
+      title: "Egypt News",
+      name: "Safaa Tawfik",
+    });
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404page", {
+    title: "404 Error",
+    name: "Safaa Tawfik",
   });
 });
 app.listen(port, () => console.log("Listening on server 3000"));
